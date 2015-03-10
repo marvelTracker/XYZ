f16c1c49249149b704c7540970c6d3192a2229d1
dinesh <dineshpdn@hotmail.com>
2015-03-10 22:51:14 +1100

    add EF

---
+++ compareIT.Data/Model/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareIT.Data.Model
+{
+    public class Computer
+    {
+        public int ComputerID { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string BaseModelProcessor { get; set; }
+        public string ProcessorSpeed { get; set; }
+        public string Brand { get; set; }
+        public string VideoCard { get; set; }
+        public string RAM { get; set; }
+        public string OperatingSystem { get; set; }
+        public string ScreenSize { get; set; }
+
+    }
+
+
+}
---
+++ compareIT.Data/compareITContext .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using compareIT.Data.Model;
+
+namespace compareIT.Data
+{
+    public class CompareITContext : DbContext
+    {
+
+        public CompareITContext()
+            : base("CompareITContext")
+        {
+        }
+        public DbSet<Computer> Computers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
