a23a978a17e89c9d04b3ae472cac3bfff775ed16
dineshpdn <dineshpdn@gmail.com>
2015-04-02 00:48:27 +1100

    test

--- CompareIT/Areas/HelpPage/ApiDescriptionExtensions.cs
+++ CompareIT/Areas/HelpPage/ApiDescriptionExtensions.cs
@@ -7,6 +7,8 @@ namespace CompareIT.Areas.HelpPage
 {
     public static class ApiDescriptionExtensions
     {
+
+
         /// <summary>
         /// Generates an URI-friendly ID for the <see cref="ApiDescription"/>. E.g. "Get-Values-id_name" instead of "GetValues/{id}?name={name}"
         /// </summary>
